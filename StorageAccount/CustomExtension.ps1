bfc08bf6cedaa336b25ab43b6eeeddbc704baddd
Ewan BURASOVITCH <burasovitch1e@gmail.com>
2024-11-03 08:46:17 -0500

    update

---
+++ StorageAccount/CustomExtension.ps1
@@ -0,0 +1,19 @@
+﻿#install Windows Server Feature for IIS service
+Add-WindowsFeature Web-Server -IncludeAllSubFeature
+Add-WindowsFeature Web-Mgmt-Service
+
+# Define the HTML content
+$htmlContent = @"
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1>Welcome to this Windows Web Server</h1>
+</body>
+</html>
+"@
+
+# Write the HTML content to index.html
+Set-Content -Path "c:\inetpub\wwwroot\index.html" -Value $htmlContent
